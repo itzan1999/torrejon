585c4b7b027325be656f6d21ef30d49c10a499db
itzan1999 <itzan.casales@gmail.com>
2026-03-30 19:08:55 +0200

    permisos a storage

--- start.sh
+++ start.sh
@@ -6,6 +6,10 @@ sleep 3
 # Dar permisos al socket para que nginx pueda accederlo
 chmod 777 /run/php-fpm.sock
 
+# Permisos de storage para sesiones y logs
+chmod -R 777 /var/www/html/storage
+chmod -R 777 /var/www/html/bootstrap/cache
+
 # Ejecutar migraciones
 php artisan config:cache
 php artisan route:cache
