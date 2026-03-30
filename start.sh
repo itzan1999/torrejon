1ba0fd00145c3350cc4f85191f61d3ecbc3e37bd
itzan1999 <itzan.casales@gmail.com>
2026-03-30 18:00:35 +0200

    correccion del script y nginx

--- start.sh
+++ start.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
+
+# Ejecutar migraciones
 php artisan config:cache
 php artisan route:cache
 php artisan migrate --force
-php-fpm83 -D
+
+# Arrancar php-fpm85 en segundo plano
+php-fpm85 -D
+
+# Esperar a que el socket esté listo
+sleep 2
+
+# Arrancar nginx en primer plano
 nginx -g 'daemon off;'
