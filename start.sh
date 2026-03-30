bc90f5aa60f81b32e303b1d0b11ee9e247597e58
itzan1999 <itzan.casales@gmail.com>
2026-03-30 18:42:35 +0200

    correccion del script

--- start.sh
+++ start.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 
+php-fpm85 --allow-to-run-as-root -D
+
+# Esperar hasta que el socket esté listo
+echo "Esperando socket php-fpm..."
+for i in $(seq 1 30); do
+    if [ -S /var/run/php-fpm.sock ]; then
+        echo "Socket listo!"
+        break
+    fi
+    sleep 1
+done
+
 # Ejecutar migraciones
 php artisan config:cache
 php artisan route:cache
 php artisan migrate --force
 php artisan db:seed --class=DatosPruebaSeeder --force
-php artisan db:seed --class=DatosLecheSeeder --force
+php artisan db:seed --class=DatosLeche --force
 
-
-# Arrancar php-fpm85 en segundo plano
-php-fpm85 --allow-to-run-as-root -D
-
-sleep 2
 
 nginx -g 'daemon off;'
