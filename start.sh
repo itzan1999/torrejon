6dcea8f60dc04039f4636d7fda5dcb474e48b15d
itzan1999 <itzan.casales@gmail.com>
2026-03-30 18:11:50 +0200

    correccion del script

--- start.sh
+++ start.sh
@@ -4,9 +4,12 @@
 php artisan config:cache
 php artisan route:cache
 php artisan migrate --force
+php artisan db:seed --class=DatosPruebaSeeder --force
+php artisan db:seed --class=DatosLecheSeeder --force
+
 
 # Arrancar php-fpm85 en segundo plano
-php-fpm85 -D
+php-fpm85 --allow-to-run-as-root -D
 
 # Esperar a que el socket esté listo
 sleep 2
