8657144aed65eb0b6f233791681096fbde3d8470
itzan1999 <itzan.casales@gmail.com>
2026-03-30 18:31:34 +0200

    correccion tras debug

--- start.sh
+++ start.sh
@@ -10,9 +10,7 @@ php artisan db:seed --class=DatosLecheSeeder --force
 
 # Arrancar php-fpm85 en segundo plano
 php-fpm85 --allow-to-run-as-root -D
-sleep 3
-echo "=== Sockets disponibles ==="
-ls /var/run/*.sock 2>/dev/null || echo "No hay sockets en /var/run/"
-ls /tmp/*.sock 2>/dev/null || echo "No hay sockets en /tmp/"
+
+sleep 2
 
 nginx -g 'daemon off;'
